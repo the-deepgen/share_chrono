1ede5c971444dcae066f74d78cf9cb1db55c5aec
Yoann <ybuzenet@gmail.com>
2021-08-28 16:49:41 +0200

    user context

--- front/src/App.js
+++ front/src/App.js
@@ -4,24 +4,35 @@ import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Chrono from "./Pages/Chrono/Chrono";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import userAPI from "./services/userAPI";
+
+// Contexts
+import userInfosContext from "./Contexts/userInfos";
 
 function App() {
   const [userName, setUserName] = useState(userAPI.getUsername());
 
-  return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        {!userName && <p>lol</p>}
-      </div>
+  // CONTEXT CREATION User Infos
+  const contextUserInfos = {
+    userName: userName,
+    setUserName: setUserName,
+  };
 
-      <Switch>
-        <Route path="/" exact render={(props) => <Home />} />
-        <Route path="/chrono/:id" render={(props) => <Chrono />} />
-      </Switch>
-    </Router>
+  return (
+    <userInfosContext.Provider value={contextUserInfos}>
+      <Router>
+        <div className="App">
+          <Navbar />
+          {!userName && <p>lol</p>}
+        </div>
+
+        <Switch>
+          <Route path="/" exact render={(props) => <Home />} />
+          <Route path="/chrono/:id" render={(props) => <Chrono />} />
+        </Switch>
+      </Router>
+    </userInfosContext.Provider>
   );
 }
 
